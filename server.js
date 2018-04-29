a759b9f63699a7771c2a2865bcbdc667648e4089
raulanatol <raul@natol.es>
2018-04-29 13:19:20 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+const PORT = 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.send('<iframe src="https://giphy.com/embed/11RwocOdukxqN2" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
+});
+
+app.listen(PORT, function () {
+    console.log('Server is running, port ' + PORT);
+});
