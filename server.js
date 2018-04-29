c5535082b5a42cab021d115be07441774b2d11e3
raulanatol <raul@natol.es>
2018-04-29 13:28:13 +0100

    Use process.env.PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
